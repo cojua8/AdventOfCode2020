b362b24bd6fc3ce7be4ad295bce1d5db47809503
Joaquin Neira <joaquin.neiraj@gmail.com>
2020-12-15 11:03:56 -0300

    day15 added

---
+++ Julia/day15-1.jl
@@ -0,0 +1,35 @@
+function memory_game(starter_numbers::Vector{<:Integer}, final_turn::Integer)
+    memory = Dict(0 => (0, 0))
+    push!(memory, Dict(number => (i, 0)
+        for (i, number) in enumerate(starter_numbers))...)
+
+
+    current_turn = length(starter_numbers)
+    last_spoken = starter_numbers[end]
+
+    for i = (current_turn + 1):final_turn
+        if memory[last_spoken][2] == 0
+            last_spoken = 0
+            memory[0] = (i, memory[0][1])
+        else
+            last_spoken = memory[last_spoken][1] - memory[last_spoken][2]
+            if haskey(memory, last_spoken)
+                memory[last_spoken] = (i, memory[last_spoken][1])
+            else
+                memory[last_spoken] = (i, 0)
+            end
+        end
+    end
+    return last_spoken
+end
+
+
+function main()
+    starter_string = read("Recursos/day15-input.txt", String)
+
+    starter_numbers = parse.(Int, split(starter_string, ","))
+
+    memory_game(starter_numbers, 2020)
+end
+
+main()
